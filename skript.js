e0fcd9ae328bdf6a1f5c35a87b5e79ebc602d6cc
Vladimir Cicmanec <vladimir.cicmanec@gmail.com>
2013-05-10 19:04:44 +0200

    cleanup, fixes

--- skript.js
+++ skript.js
@@ -5,22 +5,22 @@
 // Uses XRegExp library - the patterns are actually strings (backslashes need to be ecaped)
 // Uses XRegExp unicode addon - \\p{L} = any unicode word character (unlike js \w = [A-Za-z])
 var patterns = [
-    ["((P|p)rosperit\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((Ľ|ľ)avicov\\p{L}* vlád\\{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((P|p)rác\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((P|p)rác\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((H|h)ospodársk\\p{L}* rast\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((E|e)konomick\\p{L}* rast\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((K|k)ríz\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((Ú|ú)spe(ch|š)\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((P|p)e(n|ň)i?az\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((P|p)redražen\\p{L}*)", SIMPLE_REPLACE_PATTERN ],
-    ["((Z|z)adarmo)", SIMPLE_REPLACE_PATTERN],
-    ["((N|n)ezamestnan\\p{L}*)", SIMPLE_REPLACE_PATTERN],
-    ["((R|r)efor\\w?m\\p{L}*)", SIMPLE_REPLACE_PATTERN],
-    ["((Š|š)krt\\p{L}*)", SIMPLE_REPLACE_PATTERN],
-    ["((E|e)xpert\\p{L}*)", SIMPLE_REPLACE_PATTERN],
-    ["((O|o)dborní\\p{L}*)", SIMPLE_REPLACE_PATTERN],
+    ["((P|p)rosperit\\p{L}*)"],
+    ["((Ľ|ľ)avicov\\p{L}* vlád\\{L}*)"],
+    ["((P|p)rác\\p{L}*)"],
+    ["((P|p)rác\\p{L}*)"],
+    ["((H|h)ospodársk\\p{L}* rast\\p{L}*)"],
+    ["((E|e)konomick\\p{L}* rast\\p{L}*)"],
+    ["((K|k)ríz\\p{L}*)"],
+    ["((Ú|ú)spe(ch|š)\\p{L}*)"],
+    ["((P|p)e(n|ň)i?az\\p{L}*)"],
+    ["((P|p)redražen\\p{L}*)"],
+    ["((Z|z)adarmo)"],
+    ["((N|n)ezamestnan\\p{L}*)"],
+    ["((R|r)efor\\w?m\\p{L}*)"],
+    ["((Š|š)krt\\p{L}*)"],
+    ["((E|e)xpert\\p{L}*)"],
+    ["((O|o)dborní\\p{L}*)"],
     ["(Slovensk.*?)(republik\\p{L}*)", "$1„$2“"],
     ["((E|e)urópsk.*)(prezident\\p{L}*)", "$1„$3“"],
     ["((S|s)mer.*)(sociáln\\p{L}* demokrac\\p{L}*)", "$1„$3“"]
@@ -30,9 +30,11 @@ var patterns = [
 for (var i = 0; i < patterns.length; i++){
 
     //create new XRegExp pattern from the string
-    var pattern = new XRegExp(patterns[k][0], "g");
+    var pattern = new XRegExp(patterns[i][0], "g");
+
+    var replacePattern = patterns[i][1] == null ? SIMPLE_REPLACE_PATTERN : patterns[i][1];
 
     //replace it
-    document.body.innerHTML = XRegExp.replace(document.body.innerHTML, pattern, patterns[k][1]);
+    document.body.innerHTML = XRegExp.replace(document.body.innerHTML, pattern, replacePattern);
 }
 
