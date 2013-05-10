f2a097d6598154d00fa85064255000ba6252cd50
Vladimir Cicmanec <vladimir.cicmanec@gmail.com>
2013-05-11 00:47:50 +0200

    use the ignore case regex flag

--- skript.js
+++ skript.js
@@ -5,26 +5,26 @@
 // Uses XRegExp library - the patterns are actually strings (backslashes need to be ecaped)
 // Uses XRegExp unicode addon - \\p{L} = any unicode word character (unlike js \w = [A-Za-z])
 var patterns = [
-    ["((P|p)rosperit\\p{L}*)"],
-    ["((Ľ|ľ)avicov\\p{L}* vlád\\{L}*)"],
-    ["((P|p)rác\\p{L}*)"],
-    ["((H|h)ospodársk\\p{L}* rast\\p{L}*)"],
-    ["((E|e)konomick\\p{L}* rast\\p{L}*)"],
-    ["((K|k)ríz\\p{L}*)"],
-    ["((Ú|ú)spe(ch|š)\\p{L}*)"],
-    ["((P|p)e(n|ň)i?az\\p{L}*)"],
-    ["((P|p)redražen\\p{L}*)"],
-    ["((Z|z)adarmo)"],
-    ["((N|n)ezamestnan\\p{L}*)"],
-    ["((R|r)efor\\w?m\\p{L}*)"],
-    ["((Š|š)krt\\p{L}*)"],
-    ["((E|e)xpert\\p{L}*)"],
-    ["((O|o)dborní\\p{L}*)"],
-    ["((A|a)nalyti\\p{L}*)"],
-    ["(Slovensk.*?)(republik\\p{L}*)", "$1„$2“"],
-    ["((E|e)urópsk\\p{L}*)\\s(prezident\\p{L}*)", "$1 „$3“"],
-    ["((S|s)mer.*)(sociáln\\p{L}* demokrac\\p{L}*)", "$1„$3“"],
-    ["((P|p)rezident\\p{L}*)(.*?)(Gašparovič\\p{L}*)", "„$1“$3$4"]
+    ["(prosperit\\p{L}*)"],
+    ["(ľavicov\\p{L}* vlád\\{L}*)"],
+    ["(prác\\p{L}*)"],
+    ["(hospodársk\\p{L}* rast\\p{L}*)"],
+    ["(ekonomick\\p{L}* rast\\p{L}*)"],
+    ["(kríz\\p{L}*)"],
+    ["(úspe(ch|š)\\p{L}*)"],
+    ["(pe(n|ň)i?az\\p{L}*)"],
+    ["(predražen\\p{L}*)"],
+    ["(zadarmo)"],
+    ["(nezamestnan\\p{L}*)"],
+    ["(refor\\w?m\\p{L}*)"],
+    ["(škrt\\p{L}*)"],
+    ["(expert\\p{L}*)"],
+    ["(odborní\\p{L}*)"],
+    ["(analyti\\p{L}*)"],
+    ["(slovensk.*?)(republik\\p{L}*)", "$1„$2“"],
+    ["(európsk\\p{L}*)\\s(prezident\\p{L}*)", "$1 „$2“"],
+    ["(smer.*)(sociáln\\p{L}* demokrac\\p{L}*)", "$1„$2“"],
+    ["(prezident\\p{L}*)(.*?)(gašparovič\\p{L}*)", "„$1“$2$3"]
 ];
 
 
@@ -36,7 +36,7 @@ function realitify(lies){
     for (var i = 0; i < patterns.length; i++){
 
         //create new XRegExp pattern from the string
-        var pattern = new XRegExp(patterns[i][0], "g");
+        var pattern = new XRegExp(patterns[i][0], "gi");
 
         //if replace pattern is not defined, use the default
         var replacePattern = patterns[i][1] == null ? SIMPLE_REPLACE_PATTERN : patterns[i][1];
